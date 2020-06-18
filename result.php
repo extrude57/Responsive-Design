6265c0d06f83b0d076c7b0c32600a2734178c092
extrude57 <gamemeup8@protonmail.com>
2020-06-17 20:10:05 -0700

    Form and the results submission page for the form I do not yet know if this will work until I launch apache server on it or possibly find a neat live server plugin for php like the one I got here for vscode on port 5500 or 4500

--- result.php
+++ result.php
@@ -45,6 +45,72 @@ scrollamount="9" width="100%">Nicholas Lacapria - User Interface II </marquee>
         </div>
       </div> <!-- End container 1-->
      
+
+      <?php
+// Defensive function from SQL Injection Attacks by stripping out Uneseccary Characters that do not belong in the database or page.
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr  = "";
+$name = $email = $gender = $comment = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+
+?>
+
+<h2>You Submission</h2>
+
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
     
 
   
